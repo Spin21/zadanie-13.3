0d2b347a36e5cf22bddac0b4d886d2a84b7c0a50
Stefan Tórz <storz@interia.eu>
2018-02-01 14:37:14 +0100

    change files

--- program.js
+++ program.js
@@ -2,19 +2,19 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    var version = process.versions;
-    var node = node.version;
-    var ns = node.toString()
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        switch (instruction)
-        case '/exit': {
-            process.stdout.write('Quitting app!' + ns);
+        switch (instruction){
+        case '/exit': 
+            process.stdout.write('Quitting app!');
             process.exit();
-        } 
-        case: !'/exit' {
-            process.stdout.write('Wrong instruction!\n');
+        break;
+        case '/version':
+            process.stdout.write(process.versions.node + '\n');
+        break;
+        default:
+            process.stdout.write('wrong instruction \n');
         }
     }
 });
